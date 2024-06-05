c4755fc1a41472453958208b100284cbbfa9ec9d
Nicolas Leigh <linze201@gmail.com>
2024-06-05 21:23:29 +0800

    submit form

--- 04/web-form/src/components/SignupForm.vue
+++ 04/web-form/src/components/SignupForm.vue
@@ -1,10 +1,13 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email" />
 
     <label>Password:</label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">
+      {{ passwordError }}
+    </div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -21,6 +24,10 @@
     <div class="terms">
       <input type="checkbox" required v-model="terms" />
       <label>I agree to the terms and conditions</label>
+    </div>
+
+    <div class="submit">
+      <button type="submit">Create an Account</button>
     </div>
   </form>
   <p>Email: {{ email }}</p>
@@ -40,6 +47,7 @@ export default {
       terms: false,
       tempSkill: '',
       skills: [],
+      passwordError: '',
     };
   },
   methods: {
@@ -53,6 +61,21 @@ export default {
     },
     deleteSkill(skill) {
       this.skills = this.skills.filter((s) => s !== skill);
+    },
+    handleSubmit() {
+      // Validate the password
+      this.passwordError =
+        this.password.length > 5
+          ? ''
+          : 'Password must be at least 6 characters';
+
+      if (!this.passwordError) {
+        console.log('email:', this.email);
+        console.log('password:', this.password);
+        console.log('role:', this.role);
+        console.log('skills:', this.skills);
+        console.log('terms accepted:', this.terms);
+      }
     },
   },
 };
@@ -104,5 +127,22 @@ input[type='checkbox'] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background-color: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: tomato;
+  margin-top: 10px;
+  font-size: 0.8rem;
+  font-weight: bold;
 }
 </style>
