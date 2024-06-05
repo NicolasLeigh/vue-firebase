e6ad2e1a729eae213d4b19f23bac010439f6a2c1
Nicolas Leigh <linze201@gmail.com>
2024-06-05 17:46:37 +0800

    build the reaction timer

---
+++ 03/reaction-timer/src/components/TheResults.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>Reaction time: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja';
+    } else if (this.score < 400) {
+      this.rank = 'Samurai';
+    } else {
+      this.rank = 'Grasshopper';
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4rem;
+  color: crimson;
+  font-weight: bold;
+}
+</style>
