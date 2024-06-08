d2bc6e9e5dd19e62e4000d37f4658d3655729645
Nicolas Leigh <linze201@gmail.com>
2024-06-08 16:06:46 +0800

    composition api - ref

---
+++ 07/dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <p ref="p">My name is {{ name }} and I am {{ age }} years old</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'HomeView',
+  setup() {
+    const p = ref(null);
+
+    const name = 'mario';
+    const age = 30;
+
+    const handleClick = () => {
+      console.log(p, p.value);
+      p.value.classList.add('test');
+    };
+
+    return { name, age, handleClick, p };
+  },
+};
+</script>
