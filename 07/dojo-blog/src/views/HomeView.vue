0e109696c886482d8432bf9ff28ef0e6421f3700
Nicolas Leigh <linze201@gmail.com>
2024-06-08 16:11:40 +0800

    more common usage of ref

--- 07/dojo-blog/src/views/HomeView.vue
+++ 07/dojo-blog/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <p ref="p">My name is {{ name }} and I am {{ age }} years old</p>
+    <p>My name is {{ name }} and I am {{ age }} years old</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name" />
   </div>
 </template>
 
@@ -11,17 +13,17 @@ import { ref } from 'vue';
 export default {
   name: 'HomeView',
   setup() {
-    const p = ref(null);
+    // const p = ref(null);
 
-    const name = 'mario';
-    const age = 30;
+    const name = ref('mario');
+    const age = ref(30);
 
     const handleClick = () => {
-      console.log(p, p.value);
-      p.value.classList.add('test');
+      name.value = 'luigi';
+      age.value = 35;
     };
 
-    return { name, age, handleClick, p };
+    return { name, age, handleClick };
   },
 };
 </script>
