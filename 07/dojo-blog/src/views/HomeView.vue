08713b047a0ea4a3a7786a7295cf76c921c7a605
Nicolas Leigh <linze201@gmail.com>
2024-06-08 16:22:12 +0800

    ref and reactive

--- 07/dojo-blog/src/views/HomeView.vue
+++ 07/dojo-blog/src/views/HomeView.vue
@@ -1,29 +1,39 @@
 <template>
   <div class="home">
-    <p>My name is {{ name }} and I am {{ age }} years old</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name" />
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
-    // const p = ref(null);
+    const ninjaOne = ref({
+      name: 'Ryu',
+      age: 25,
+    });
+    // cannot use primitive values with reactive
+    const ninjaTwo = reactive({
+      name: 'Yoshi',
+      age: 35,
+    });
 
-    const name = ref('mario');
-    const age = ref(30);
-
-    const handleClick = () => {
-      name.value = 'luigi';
-      age.value = 35;
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 30;
+    };
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 40;
     };
 
-    return { name, age, handleClick };
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
   },
 };
 </script>
