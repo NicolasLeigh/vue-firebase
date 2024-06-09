f48262b7db0585abae86eb1468ac9d0f75c4668c
Nicolas Leigh <linze201@gmail.com>
2024-06-09 17:52:27 +0800

    useRouter and useRoute

--- 07/dojo-blog/src/views/Create.vue
+++ 07/dojo-blog/src/views/Create.vue
@@ -15,12 +15,15 @@
 
 <script>
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 export default {
   setup() {
     const title = ref("");
     const body = ref("");
     const tag = ref("");
     const tags = ref([]);
+
+    const router = useRouter();
 
     const handleKeydown = () => {
       if (!tags.value.includes(tag.value)) {
@@ -37,6 +40,7 @@ export default {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(post),
       });
+      router.push({ name: "Home" });
     };
 
     return { title, body, tag, handleKeydown, tags, handleSubmit };
--- 07/dojo-blog/src/views/Details.vue
+++ 07/dojo-blog/src/views/Details.vue
@@ -14,11 +14,14 @@
 <script>
 import getPost from "../composables/getPost";
 import Spinner from "../components/Spinner.vue";
+import { useRoute } from "vue-router";
 export default {
   props: ["id"],
   components: { Spinner },
   setup({ id }) {
-    const { post, error, load } = getPost(id);
+    const route = useRoute();
+    // const { post, error, load } = getPost(id);
+    const { post, error, load } = getPost(route.params.id);
     load();
     return { post, error };
   },
