4b262ec633a1c60100af6f2961b82fa542c7a333
Nicolas Leigh <linze201@gmail.com>
2024-06-09 15:56:14 +0800

    async fetch data

--- 07/dojo-blog/src/views/HomeView.vue
+++ 07/dojo-blog/src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList v-if="showPosts" :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
     <button @click="showPosts = !showPosts">toggle posts</button>
     <button @click="posts.pop()">delete a post</button>
   </div>
@@ -16,17 +20,24 @@ export default {
     PostList,
   },
   setup() {
-    const posts = ref([
-      {
-        title: 'welcome to my blog',
-        body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima voluptatum doloribus hic eveniet atque enim id nihil error libero aliquam laudantium beatae, voluptas sequi! Harum sapiente fuga esse! Consequuntur nesciunt quam rerum quod odio blanditiis beatae, sunt totam dolore ipsa perspiciatis ut molestiae aliquam sint vel provident ab nulla repellendus vero. Fugiat aliquam blanditiis neque. Quis dolorum quidem iusto iure vel optio dolor sit modi libero, voluptates vero, quibusdam magnam ex, distinctio velit maxime excepturi dicta. Optio quam temporibus libero? Odit ratione unde sunt tenetur fugiat maxime aliquam in, adipisci quisquam quam natus numquam dolor magni molestias libero obcaecati assumenda quidem. Voluptate vel tempore dignissimos, unde quidem aspernatur recusandae autem repellendus quis, quisquam ipsam sequi quae deleniti? Ipsa perferendis sint maiores illo quia explicabo eaque corrupti unde iure ex commodi aspernatur sunt laborum minus, harum repellat. Excepturi corrupti possimus eligendi praesentium totam ea consequuntur iure, fugit quisquam ducimus rem maxime, at ad earum. Veritatis animi amet voluptatum praesentium nihil aspernatur natus repellendus quibusdam esse voluptate ea ut quasi accusantium exercitationem est, aperiam enim error saepe provident unde. Ratione unde culpa dolorum animi perferendis magnam! Accusantium deserunt illum laborum dolores dolorum architecto saepe natus libero voluptates, ad facere, dolor harum nostrum',
-        id: 1,
-      },
-      { title: 'vue 3 is here', body: 'lorem ipsum...', id: 2 },
-    ]);
+    const posts = ref([]);
+    const error = ref(null);
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts');
+        if (!data.ok) throw Error('no data available');
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.log(error.value);
+      }
+    };
+
+    load();
 
     const showPosts = ref(true);
-    return { posts, showPosts };
+    return { posts, showPosts, error };
   },
 };
 </script>
