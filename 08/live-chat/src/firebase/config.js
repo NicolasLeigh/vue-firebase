3a2cacf50262a88a7fc4d578cc1f8e58e899b99d
Nicolas Leigh <linze201@gmail.com>
2024-06-10 18:17:06 +0800

    setup firebase

---
+++ 08/live-chat/src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS_2GM7xgTpxIXDdioSqFVwhJx3b3OVcE",
+  authDomain: "vue-firebase-407bf.firebaseapp.com",
+  projectId: "vue-firebase-407bf",
+  storageBucket: "vue-firebase-407bf.appspot.com",
+  messagingSenderId: "822196807689",
+  appId: "1:822196807689:web:1c11086e834a3d7033f0d8",
+};
+
+// init firebase
+const app = initializeApp(firebaseConfig);
+
+// init firestore service
+const db = getFirestore(app);
+
+export { db };
