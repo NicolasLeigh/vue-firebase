bc80c4a8a544ad5dd2ec8fbd19731cf29391650a
Nicolas Leigh <linze201@gmail.com>
2024-06-11 17:44:55 +0800

    create login functionality

---
+++ 08/live-chat/src/composables/useLogin.js
@@ -0,0 +1,22 @@
+import { auth } from "@/firebase/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { ref } from "vue";
+
+const error = ref(null);
+
+const login = async (email, password) => {
+  error.value = null;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.log(err.message);
+    error.value = "Incorrect login credentials";
+  }
+};
+
+const useLogin = () => {
+  return { error, login };
+};
+
+export default useLogin;
