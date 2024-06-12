7b0f33c2f8633b451feb18c412a3b4ae3766184c
Nicolas Leigh <linze201@gmail.com>
2024-06-12 22:37:34 +0800

    auto-scroll to the bottom of the chat window

--- 08/live-chat/src/components/ChatWindow.vue
+++ 08/live-chat/src/components/ChatWindow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <div v-if="documents" class="messages">
+    <div v-if="documents" class="messages" ref="messages">
       <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
         <span class="created-at">{{ doc.createdAt }} </span>
         <span class="name">{{ doc.name }} </span>
@@ -14,7 +14,7 @@
 <script>
 import getCollection from "../composables/getCollection";
 import { formatDistanceToNow } from "date-fns";
-import { computed } from "vue";
+import { computed, ref, onUpdated } from "vue";
 
 export default {
   setup() {
@@ -28,7 +28,14 @@ export default {
         });
     });
 
-    return { error, documents, formattedDocuments };
+    // auto-scroll to the bottom of the chat window
+    const messages = ref(null);
+
+    onUpdated(() => {
+      messages.value.scrollTop = messages.value.scrollHeight;
+    });
+
+    return { error, documents, formattedDocuments, messages };
   },
 };
 </script>
