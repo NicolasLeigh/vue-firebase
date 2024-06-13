082195d288efbbc5119fda4b854e160562e906ea
Nicolas Leigh <linze201@gmail.com>
2024-06-13 19:38:49 +0800

    using watchEffect to unsubscribe onSnapshot when component is unmounted

--- 08/live-chat/src/composables/getCollection.js
+++ 08/live-chat/src/composables/getCollection.js
@@ -1,15 +1,16 @@
 import { db } from "@/firebase/config";
 import { collection, getDoc, onSnapshot, orderBy, query } from "firebase/firestore";
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 
 const getCollection = (coll) => {
   const documents = ref(null);
   const error = ref(null);
 
   let q = query(collection(db, coll), orderBy("createdAt"));
-  onSnapshot(
+  const unsubscribe = onSnapshot(
     q,
     (snap) => {
+      console.log("Snapshot");
       let results = [];
       snap.docs.forEach((doc) => {
         // wait for the server to create a timestamp & send it back
@@ -24,6 +25,11 @@ const getCollection = (coll) => {
       error.value = "could not fetch the data";
     }
   );
+
+  watchEffect((onInvalidate) => {
+    // unsubscribe from prev collection when watcher is stopped (component unmounted)
+    onInvalidate(() => unsubscribe());
+  });
 
   return { documents, error };
 };
