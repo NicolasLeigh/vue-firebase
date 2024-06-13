bd1e8d9768e104c434a7f34368676253b58f5213
Nicolas Leigh <linze201@gmail.com>
2024-06-13 19:45:02 +0800

    when user is logged in, redirect to the Chatroom page

--- 08/live-chat/src/router/index.js
+++ 08/live-chat/src/router/index.js
@@ -6,9 +6,18 @@ import { auth } from "@/firebase/config";
 // auth guard
 const requireAuth = (to, from, next) => {
   let user = auth.currentUser;
-  console.log("current user in auth guard: ", user);
+  // console.log("current user in auth guard: ", user);
   if (!user) {
     next({ name: "Welcome" });
+  } else {
+    next();
+  }
+};
+
+const requireNoAuth = (to, from, next) => {
+  let user = auth.currentUser;
+  if (user) {
+    next({ name: "Chatroom" });
   } else {
     next();
   }
@@ -19,6 +28,7 @@ const routes = [
     path: "/",
     name: "Welcome",
     component: Welcome,
+    beforeEnter: requireNoAuth,
   },
   {
     path: "/chatroom",
