92568aeafe56cb9a78dc88747afbf88bd96bdbff
Nicolas Leigh <linze201@gmail.com>
2024-06-14 16:19:04 +0800

    add signup form

---
+++ 09/muso-ninjas/src/views/auth/Signup.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Sign up</h3>
+    <input type="text" placeholder="Display name" v-model="displayName" />
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div v-if="error" class="error">{{ error }}</div>
+    <button :disabled="isPending">Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+import useSignup from "../../composables/useSignup";
+export default {
+  setup() {
+    const { error, signup, isPending } = useSignup();
+    const email = ref("");
+    const password = ref("");
+    const displayName = ref("");
+
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value);
+      if (!error.value) console.log("signed up");
+    };
+
+    return { email, password, displayName, error, isPending, handleSubmit };
+  },
+};
+</script>
+
+<style></style>
