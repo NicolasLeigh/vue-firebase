107e130cc62540d1ae94d6373f60398437ccd38b
Nicolas Leigh <linze201@gmail.com>
2024-06-15 21:16:25 +0800

    create ListView at home page

--- 09/muso-ninjas/src/views/HomeView.vue
+++ 09/muso-ninjas/src/views/HomeView.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <div v-if="error" class="error">Could not fetch the data</div>
     <div v-if="documents">
-      <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+      <div v-for="doc in documents" :key="doc.id">
+        <ListView :playlists="documents" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import getCollection from "../composables/getCollection";
+import ListView from "../components/ListView.vue";
 export default {
   name: "HomeView",
-  components: {},
+  components: { ListView },
   setup() {
     const { error, documents } = getCollection("playlists");
 
