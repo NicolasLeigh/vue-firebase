42144a8a0d97328a0256306b49e24bab5d920876
Nicolas Leigh <linze201@gmail.com>
2024-06-17 21:07:28 +0800

    complete UserPlaylists component and display username at navbar

--- 09/muso-ninjas/src/components/Navbar.vue
+++ 09/muso-ninjas/src/components/Navbar.vue
@@ -7,6 +7,7 @@
         <div v-if="user">
           <router-link :to="{ name: 'createPlaylist' }">Create Playlist</router-link>
           <router-link :to="{ name: 'userPlaylists' }">My Playlist</router-link>
+          <span>Hi there, {{ user.displayName }} </span>
           <button @click="handleClick">Logout</button>
         </div>
         <div v-else>
@@ -63,5 +64,12 @@ button {
 }
 nav img {
   max-height: 60px;
+}
+span {
+  font-size: 14px;
+  display: inline-block;
+  margin-left: 16px;
+  padding-left: 16px;
+  border-left: 1px solid #eee;
 }
 </style>
--- 09/muso-ninjas/src/views/playlists/UserPlaylists.vue
+++ 09/muso-ninjas/src/views/playlists/UserPlaylists.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="user-playlists">
     <h2>My Playlists</h2>
-    <div v-if="playlists">{{ playlists[0].title }}</div>
+    <div v-if="playlists">
+      <ListView :playlists="playlists" />
+    </div>
+    <router-link :to="{ name: 'createPlaylist' }" class="btn">Create a New Playlist</router-link>
   </div>
 </template>
 
 <script>
 import getCollection from "../../composables/getCollection";
 import getUser from "../../composables/getUser";
+import ListView from "../../components/ListView.vue";
 export default {
+  components: { ListView },
   setup() {
     const { user } = getUser();
     const { documents: playlists } = getCollection("playlists", ["userId", "==", user.value.uid]);
